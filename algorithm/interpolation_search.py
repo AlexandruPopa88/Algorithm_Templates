cd6b324229f3f03275dd865ea55436aaad6baf01
Alex Popa <alex.popa@advocatus.ro>
2022-06-15 10:56:22 +0300

    Added interpolation search

---
+++ algorithm/interpolation_search.py
@@ -0,0 +1,26 @@
+"""
+Interpolation Search
+Complexity O(log log n) if list respects input, else O(n)
+Input: list - sorted, uniformly distributed
+       val - element to search for
+Output: index of val in list or -1
+"""
+
+def InterpolationSearch(lys, val):
+    low = 0
+    high = len(lys) - 1
+
+    while low <= high and val >= lys[low] and val <= lys[high]:
+        index = low + int(
+            ((high - low) / (lys[high] - lys[low])) * (val - lys[low])
+        )
+
+        if lys[index] == val:
+            return index
+
+        if lys[index] < val:
+            low = index + 1
+        else:
+            high = index - 1
+
+    return -1
